bc6185699533b3fffe6d1f40902c36be819e465f
Juan <juan.burillo@qaracter.com>
2025-02-06 12:43:02 +0000

    feat: document API

--- src/main/java/com/qaracter/digitalwallet/controller/WalletController.java
+++ src/main/java/com/qaracter/digitalwallet/controller/WalletController.java
@@ -2,6 +2,8 @@ package com.qaracter.digitalwallet.controller;
 
 import com.qaracter.digitalwallet.model.Currency;
 import com.qaracter.digitalwallet.service.WalletService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,8 +18,17 @@ public class WalletController {
         this.walletService = walletService;
     }
 
+    @Operation(summary = "Create a new wallet for a user",
+            description = "Creates a wallet with the specified currency and balance for a given user.")
     @PostMapping
-    public String createWallet(@RequestParam Long userId, @RequestParam Currency currency, @RequestParam Double balance) {
+    public String createWallet(@RequestParam
+                               @Schema(description = "The user ID for whom the wallet is created", example = "1") Long userId,
+
+                               @RequestParam
+                               @Schema(description = "The currency type for the wallet", example = "EUR") Currency currency,
+
+                               @RequestParam
+                               @Schema(description = "The initial balance for the wallet", example = "1000.0") Double balance) {
         walletService.createWallet(userId, currency, balance);
         return "Wallet added successfully";
     }
