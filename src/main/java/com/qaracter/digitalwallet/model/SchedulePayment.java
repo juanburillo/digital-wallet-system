6cc68bf9cdc0e37394f6d368a09a9156182b555a
ML-RefVm-358739\usuario.qaracter <jesus.hernandez@imagina-bpo.es>
2025-02-06 11:18:05 +0000

    feat: Modified schedule payment model to accept a transaction

--- src/main/java/com/qaracter/digitalwallet/model/SchedulePayment.java
+++ src/main/java/com/qaracter/digitalwallet/model/SchedulePayment.java
@@ -5,24 +5,19 @@ import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 public class SchedulePayment extends Thread{
+    private Transaction transaction;
     private Long id;
-    private Wallet sendingWallet;
-    private Wallet receivingWallet;
-    private double amount;
     private int days;
     private final Timer timer = new Timer();
     private final TimerTask task = new TimerTask() {
         @Override
         public void run() {
-            sendingWallet.sendMoney(receivingWallet, amount);
+            System.out.println("Money Transfers Successfully");
         }
     };
 
-    public SchedulePayment(Long id, Wallet sendingWallet, Wallet receivingWallet, double amount, int days){
-        this.id = id;
-        this.sendingWallet = sendingWallet;
-        this.receivingWallet = receivingWallet;
-        this.amount = amount;
+    public SchedulePayment(Long id, Transaction transaction, int days){
+        this.transaction = transaction;
         this.days = days;
     }
 
@@ -34,29 +29,6 @@ public class SchedulePayment extends Thread{
         this.id = id;
     }
 
-    public Wallet getSendingWallet() {
-        return sendingWallet;
-    }
-
-    public void setSendingWallet(Wallet sendingWallet) {
-        this.sendingWallet = sendingWallet;
-    }
-
-    public Wallet getReceivingWallet() {
-        return receivingWallet;
-    }
-
-    public void setReceivingWallet(Wallet receivingWallet) {
-        this.receivingWallet = receivingWallet;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
 
     public int getDays() {
         return days;
@@ -71,8 +43,7 @@ public class SchedulePayment extends Thread{
         while (true){
             try{
                 timer.schedule(task, TimeUnit.DAYS.toMillis(days));
-                System.out.println("The user " + sendingWallet.getUser() + " will be giving " + receivingWallet.getUser() +
-                        " the amount of " + amount + sendingWallet.getCurrency() + " in " + days + " days.");
+                System.out.println("Send " + transaction.getAmount() + transaction.getCurrency() + " to user");
             } catch (Exception e) {
                 System.err.println("Error caught in the thread execution");
                 e.printStackTrace();
